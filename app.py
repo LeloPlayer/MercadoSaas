998b6488a756989ecea0ff1d1cabaf4855624bc5
Samuel <samueleliasps4@gmail.com>
2026-04-17 20:06:20 -0300

    Definindo a conexao com o BD

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ import json
 from datetime import datetime, timedelta
 import os
 import random
+
+app = Flask(__name__, static_folder='static')
+CORS(app)
+
+DB_PATH = 'mercado.db'
+
+def get_db():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
